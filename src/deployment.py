2729e00d543464c751fd9a180311cb8e81cd33b9
_Arlo <31625356+ArloTGG@users.noreply.github.com>
2026-05-02 15:25:43 -0500

    Add files via upload

--- src/deployment.py
+++ src/deployment.py
@@ -1,18 +1,27 @@
 import subprocess
 import requests
 import json
+from google import genai
+from google.genai import types
+import pathlib
 
 def get_latest_logs():
-    # Pulls the last event from audit logs specifically for our 'honeyfile_trap'
-    raw_log = subprocess.check_output("ausearch -k honeyfile_trap --start recent -i | tail -n 20", shell=True)
-    return raw_log.decode('utf-8')
-
-def analyze_with_llama(log_data):
-    url = "https://api.llama-provider.com/v1/completions" # Replace with your actual endpoint
-    headers = {"Authorization": "Bearer YOUR_API_KEY"}
-    
-    prompt = f"Analyze these Linux audit logs. Differentiate between an accidental file open and malicious activity. Logs: {log_data}"
-    
-    # This is where your $110/month budget goes
-    response = requests.post(url, headers=headers, json={"prompt": prompt, "model": "llama-3-70b"})
-    return response.json()
+    # Pulls the last event from audit logs specifically for our 'honeyfile_trap'   
+    raw_log = subprocess.check_output("ausearch -k honeyfile_trap --start recent -i | tail -n 200", shell=True)
+    return raw_log
+   
+def honeypot_analysis():
+    client = genai.Client(api_key = "AIzaSyAmUEAlRBaDZkFxD6SwdKh6mpYt6TSvAY0")
+    log_data = get_latest_logs()
+    response = client.models.generate_content (
+        model = "gemini-3.1-flash-lite-preview",
+        contents = [
+                    types.Part.from_bytes(
+                    data = log_data,
+                    mime_type = "text/plain",
+                    ),
+                    "The provided logs is from a honeypot system on linux. Analyze the provided audit log and list the activity recorded tied to the users in a structured format."]
+)
+    print(response.text)
+if __name__ == "__main__":
+    honeypot_analysis()
